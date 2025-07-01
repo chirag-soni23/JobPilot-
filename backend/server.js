f26c6440e311fc359373bc973a0c5bb4d7a4003e
Chirag Soni <csoni0693@gmail.com>
2025-07-01 16:25:38 +0530

    cors settings

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,33 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { connectDb } from './database/db.js';
-import userRoutes from './routes/userRoutes.js';
-import jobRoutes from './routes/jobRoutes.js';
+import express from "express";
+import dotenv from "dotenv";
+import { connectDb } from "./database/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import jobRoutes from "./routes/jobRoutes.js";
 import jobApplyRoutes from "./routes/jobApplyRoutes.js";
 import nodemailerRoutes from "./routes/nodemailer.route.js";
-import cookieParser from 'cookie-parser'
-import cors from 'cors';
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 const PORT = 5000 || process.env.PORT;
 dotenv.config();
 
-
 // middleware
-app.use(cors());
+app.use(cors({
+  origin:["http://localhost:5173",""],credentials:true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
 // routes
-app.use('/api/user', userRoutes);
-app.use('/api/job',jobRoutes);
-app.use('/api/apply',jobApplyRoutes);
-app.use("/api/mail",nodemailerRoutes);
-
+app.use("/api/user", userRoutes);
+app.use("/api/job", jobRoutes);
+app.use("/api/apply", jobApplyRoutes);
+app.use("/api/mail", nodemailerRoutes);
 
 // server
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running at http://localhost:${PORT}`);
   connectDb();
 });
