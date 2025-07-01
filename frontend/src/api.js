3da00e9bfdf517b2f6adc14ebc4c4fe76d5f9b49
Chirag Soni <csoni0693@gmail.com>
2025-07-01 16:36:51 +0530

    cors settings

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,13 +1,46 @@
-import axios from "axios";
+import express from "express";
+import dotenv from "dotenv";
+import { connectDb } from "./database/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import jobRoutes from "./routes/jobRoutes.js";
+import jobApplyRoutes from "./routes/jobApplyRoutes.js";
+import nodemailerRoutes from "./routes/nodemailer.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true,   // ✅ Important for sending cookies like token
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://job-pilot-rho.vercel.app", 
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// ✅ Routes
+app.use("/api/user", userRoutes);
+app.use("/api/job", jobRoutes);
+app.use("/api/apply", jobApplyRoutes);
+app.use("/api/mail", nodemailerRoutes);
+
+// ✅ Server start
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+  connectDb();
 });
-
-const token = localStorage.getItem("token");
-if (token) {
-  api.defaults.headers.common.Authorization = `Bearer ${token}`;
-}
-
-export default api;
