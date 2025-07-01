b01738ad4dd324bfbbcb2942e0881e8f5506b6b6
Chirag Soni <csoni0693@gmail.com>
2025-07-01 17:26:35 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ dotenv.config();
 // middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://job-pilot-six.vercel.app"],
+    origin: ["http://localhost:5173", "https://job-pilot-ten.vercel.app"],
     credentials: true,
   })
 );
