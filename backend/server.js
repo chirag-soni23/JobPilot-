108c7bf1763a0d9a33ebb6a4f1e577ece2a6e050
Chirag Soni <csoni0693@gmail.com>
2025-07-01 16:28:13 +0530

    cors settings

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ dotenv.config();
 
 // middleware
 app.use(cors({
-  origin:["http://localhost:5173",""],credentials:true
+  origin:["http://localhost:5173","https://job-pilot-rho.vercel.app"],credentials:true
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
