51a40b1681cb52eb77e4d9994a0d6185d4d6e6b6
Chirag Soni <csoni0693@gmail.com>
2025-07-01 16:42:25 +0530

    cors settings

--- backend/server.js
+++ backend/server.js
@@ -8,25 +8,13 @@ import nodemailerRoutes from "./routes/nodemailer.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+const app = express();
+const PORT = 5000 || process.env.PORT;
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://job-pilot-rho.vercel.app", 
-];
-
-//  CORS Setup
+// middleware
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: ["http://localhost:5173", "https://job-pilot-rho.vercel.app"],
   credentials: true,
 }));
 
@@ -34,13 +22,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// Routes
+// routes
 app.use("/api/user", userRoutes);
 app.use("/api/job", jobRoutes);
 app.use("/api/apply", jobApplyRoutes);
 app.use("/api/mail", nodemailerRoutes);
 
+// server
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
   connectDb();
 });
