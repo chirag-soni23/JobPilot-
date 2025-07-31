7c8d8a9978096d24614e0e52281bbef69ebaa7b7
Chirag Soni <csoni0693@gmail.com>
2025-07-31 14:26:29 +0530

    add production

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,13 +2,13 @@ import jwt from "jsonwebtoken";
 
 export const generateToken = (id, res) => {
   const token = jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "15d"
+    expiresIn: "15d",
   });
 
   res.cookie("token", token, {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-     secure: process.env.NODE_ENV === "production",
-    sameSite: "lax"
+    secure: true,
+    sameSite: "lax",
   });
 };
