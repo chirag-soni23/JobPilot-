84de893d9c4eebd6fa1f54a4ab77d3975f0c37e7
Chirag Soni <csoni0693@gmail.com>
2025-07-31 14:21:59 +0530

    add production

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,7 +8,7 @@ export const generateToken = (id, res) => {
   res.cookie("token", token, {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    secure: true,
-    sameSite: "strict"
+     secure: process.env.NODE_ENV === "production",
+    sameSite: "lax"
   });
 };
