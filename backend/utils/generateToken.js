b115e5461f262216a849d5942678ad1d914099fd
Chirag Soni <csoni0693@gmail.com>
2025-08-02 09:57:24 +0530

    bug fix

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,6 +7,7 @@ export const generateToken = (id, res) => {
   res.cookie("token", token, {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true,
+    secure: true,
     sameSite: "none",
   });
 };
