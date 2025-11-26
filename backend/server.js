930efbfa186270bf7dfe469ef1cabced70bce30e
Chirag Soni <csoni0693@gmail.com>
2025-11-26 22:22:10 +0530

    some updates

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,22 @@ import nodemailerRoutes from "./routes/nodemailer.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import compression from "compression";
+import helmet from "helmet"; 
 
 const app = express();
 const PORT = 5000;
 dotenv.config();
 
-// middleware
+app.set("trust proxy", 1);
+
+app.use(
+  helmet({
+    crossOriginOpenerPolicy: { policy: "same-origin-allow-popups" },
+    crossOriginEmbedderPolicy: false, 
+    contentSecurityPolicy: false,
+  })
+);
+
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://job-pilot-nu.vercel.app"],
@@ -30,13 +40,11 @@ app.get("/", (req, res) => {
   res.json("Backend is Live!");
 });
 
-// routes
 app.use("/api/user", userRoutes);
 app.use("/api/job", jobRoutes);
 app.use("/api/apply", jobApplyRoutes);
 app.use("/api/mail", nodemailerRoutes);
 
-// server
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running at http://localhost:${PORT}`);
   connectDb();
