ff2891ff6c6bb28898f703e27585debfd5eae103
Chirag Soni <csoni0693@gmail.com>
2025-11-28 12:22:03 +0530

    updated changes

--- backend/routes/nodemailer.route.js
+++ backend/routes/nodemailer.route.js
@@ -9,7 +9,10 @@ route.post("/send-email", async (req, res) => {
   const { name, email, phone, message } = req.body;
 
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
