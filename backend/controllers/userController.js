47531c09a06a4531679aa88036b42835de372d7d
Chirag Soni <csoni0693@gmail.com>
2025-11-28 10:48:25 +0530

    add

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -107,7 +107,7 @@ export const userProfile = TryCatch(async (req, res) => {
 
 export const logout = TryCatch(async (req, res) => {
   res.cookie("token", "", {
-    maxAge: 15 * 24 * 60 * 60 * 1000,
+    maxAge: 0,
     httpOnly: true,
     secure: true,
     sameSite: "none",
