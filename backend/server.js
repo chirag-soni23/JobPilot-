ccf0c71225851d5c9e0069fa94fe17ac6379002f
Chirag Soni <csoni0693@gmail.com>
2025-11-28 14:55:26 +0530

    updated chnages

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,8 @@ const PORT = process.env.PORT || 5000;
 
 app.set("trust proxy", 1);
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 app.use(cookieParser());
 
 const ALLOWED_ORIGINS = [
